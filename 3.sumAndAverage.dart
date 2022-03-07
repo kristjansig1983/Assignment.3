1846001524bb0f1a0935ea6755f3a8295b688371
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-03-07 20:38:56 +0000

    finished2
    Búin með 2 liði.

---
+++ 3.sumAndAverage.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main() {
+  //Sum and avg. of five numbers
+  int sum = 0;
+  for(int i = 1; i <= 5; i++){
+    print('Insert ${i}.number');
+    int userNum = int.parse(stdin.readLineSync());
+    int sumOf = sum += userNum;
+    double avg = sumOf / 5;
+  }
+  print('sum of these numbers are: ${sumOf}');
+
+}
