608ef16dda1aeec46f0d3b329e4ec506611a68ed
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-03-08 10:52:18 +0000

    Klárað
    Kláraði verkefnið!

--- 3.sumAndAverage.dart
+++ 3.sumAndAverage.dart
@@ -5,14 +5,13 @@ void main() {
   //Sum and avg. of five numbers
 
   int sum = 0;
-  for (int i = 1; i <= 5; i++) {
+  for(int i = 1; i <= 5; i++){
     print('Insert ${i}.number');
-    int sumOf = sum += int.parse(stdin.readLineSync());
-    double avg = sumOf / 5;
-    
-    print('Sum of these numbers are: ${sumOf}');
-
-    print('Avarage of these numbers are: ${avg}');
+    sum += int.parse(stdin.readLineSync());
   }
+  int sumOf = sum;
+  double avg = sumOf / 5;
+  print('Sum of these numbers is: ${sumOf}');
+  print('Average of these numbers is: ${avg}');
 }
 
