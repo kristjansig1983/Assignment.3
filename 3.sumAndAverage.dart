0eb73bc10ad8475693913deb68462cc2c90ee319
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-03-08 10:41:18 +0000

    Finished 3
    Búinn með 3 liði, einn eftir.

--- 3.sumAndAverage.dart
+++ 3.sumAndAverage.dart
@@ -3,13 +3,16 @@ import 'dart:io';
 
 void main() {
   //Sum and avg. of five numbers
-  int sum = 0;
-  for(int i = 1; i <= 5; i++){
-    print('Insert ${i}.number');
-    int userNum = int.parse(stdin.readLineSync());
-    int sumOf = sum += userNum;
-    double avg = sumOf / 5;
-  }
-  print('sum of these numbers are: ${sumOf}');
 
+  int sum = 0;
+  for (int i = 1; i <= 5; i++) {
+    print('Insert ${i}.number');
+    int sumOf = sum += int.parse(stdin.readLineSync());
+    double avg = sumOf / 5;
+    
+    print('Sum of these numbers are: ${sumOf}');
+
+    print('Avarage of these numbers are: ${avg}');
+  }
 }
+
---
+++ 4.countNomberOfDigits.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  print('Enter a number:');
+  double userInput = double.parse(stdin.readLineSync());
+  int count = 0;
+  while(userInput >= 1) {
+    userInput /=10;
+    count++;
+  }
+  print('Number of digits is: ${count}');
+}
